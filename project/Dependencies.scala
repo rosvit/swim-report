2750e9db80c2faf703aef2fad06c17124e4980c8
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-09-07 09:40:17 +0200

    Update fs2-core to 3.14.0 (#83)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val catsVersion = "2.13.0"
   val catsEffectVersion = "3.7.1"
   val declineVersion = "2.6.2"
-  val fs2Version = "3.13.0"
+  val fs2Version = "3.14.0"
   val circeVersion = "0.14.16"
   val fitSdkVersion = "21.214.0"
   val scalaTestVersion = "3.2.20"
